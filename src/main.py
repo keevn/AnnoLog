0a6d4508cb78ffdd89d234a376896086f06775a2
Hua Wang <keevn@163.com>
2021-06-21 07:06:23 -0400

    add parseFact static function in fact

--- src/main.py
+++ src/main.py
@@ -10,9 +10,6 @@ path = f'{cwd}{sepa}'
 codefile_name = input('code file : ')
 name = path+codefile_name
 
-fact_pattern = re.compile(r'([a-z][a-z|\d|_]*)\(([a-z][a-z|\d|_]*)([,[a-z][a-z|\d|_]*]*)\)([@[a-z][a-z|\d|_]*]?)?\.')
-rule_pattern = re.compile(r'([a-z][a-z|\d|_]*\([a-z|\d|A-Z|_|,]*\)(?:[@[a-z|A-Z][a-z|\d|+|_|A-Z]*]?)?):-([a-z][a-z|\d|_]*\([a-z|\d|A-Z|_|,]*\)(?:[@[a-z|A-Z][a-z|\d|+|_|A-Z]*]?)?)(,[a-z][a-z|\d|_]*\([a-z|\d|A-Z|_|,]*\)(?:[@[a-z|A-Z][a-z|\d|+|_|A-Z]*]?)?)?\.')
-
 factList = []
 contextList = []
 rules = []
@@ -27,10 +24,4 @@ for line in fileinput.input(files=name):
 for f in factList:
     print(f)
 
-t = 'aasdf_adsf(asdf304Adsf,dfd)@C+W:-aasdfadsf(asdfdsf,Y)@A,adfs(ASD,DE),fffdg(asdf,DDf)@C.'
-rule_pattern = re.compile(r'([a-z][a-z|\d|_]*\([a-z|\d|A-Z|_|,]*\)(?:[@[a-z|A-Z][a-z|\d|+|_|A-Z]*]?)?):-([a-z][a-z|\d|_]*\([a-z|\d|A-Z|_|,]*\)(?:[@[a-z|A-Z][a-z|\d|+|_|A-Z]*]?)?)(,[a-z][a-z|\d|_]*\([a-z|\d|A-Z|_|,]*\)(?:[@[a-z|A-Z][a-z|\d|+|_|A-Z]*]?)?)*\.')
-
-m = rule_pattern.match(t)
-if m:
-    print(m.groups())
 
