f797619a44bea822be46bd4777335792c6b7108d
Hua Wang <keevn@163.com>
2021-06-22 00:14:26 -0400

    sync requirements.txt file within this project

---
+++ src/test/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='AnnoLog',
+    version='1.0',
+    packages=['test', 'AnnoLog'],
+    package_dir={'': 'src/test'},
+    url='https://github.com/keevn/AnnoLog.git',
+    license='',
+    author='Keevn',
+    author_email='hu_wan@live.concordia.ca',
+    description='COMP 6591 Project'
+)
